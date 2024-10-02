fdabe79005e5bba701990d2486216fd447f66edb
Thomas Meyers <funclemoz@gmail.com>
2024-10-02 14:09:37 -0600

    Added classes Job and Resume

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2000;
+        job1._endYear = 2015;
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Computer Engineer";
+        job2._startYear = 2015;
+        job2._endYear = 2022;
+
+        job1.Display();
+
+        job2.Display();
+
+
+        Resume myResume = new Resume();
+        myResume._name = "Kirk Russell";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
