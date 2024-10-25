ceebf8327c384c38a2ffae158801466bde214c43
Thomas Meyers <funclemoz@gmail.com>
2024-10-25 01:22:57 -0600

    removed comments

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -11,17 +11,13 @@ public class Scripture
         "In all thy ways acknowledge him, and he shall direct thy paths."
     };
 
-    //private Dictionary<int, (string[] words, List<bool> hiddenStates)> hiddenWords;
     private Dictionary<int, List<Word>> hiddenWords;
     public Scripture()
     {
-        //hiddenWords = new Dictionary<int, (string[], List<bool>)>();
         hiddenWords = new Dictionary<int, List<Word>>();
 
         for (int i = 0; i < verses.Count; i++)
         {
-            //string[] words = verses[i].Split(" ");
-            //hiddenWords[i] = (words, new List<bool>(new bool[words.Length]));
             var words = verses[i].Split(" ").Select(word => new Word(word)).ToList();
 
             hiddenWords[i] = words;
@@ -34,18 +30,9 @@ public class Scripture
             throw new ArgumentOutOfRangeException("Invalid verse index");
         }
 
-        //var (words, hiddenStates)= hiddenWords[verseIndex];
         var words = hiddenWords[verseIndex];
 
-        //List<int> indices = Enumerable.Range(0, words.Length).Where(i => !hiddenStates[i]).ToList();
         List<int> indices = Enumerable.Range(0, words.Count).Where(i => !words[i].IsHidden()).ToList();
-
-        //Random random = new Random();
-        //List<int> selectedIndices = indices.OrderBy(x => random.Next()).Take(3).ToList();
-
-        //HideAvailableWords(indices, words, hiddenStates);
-
-        //hiddenWords[verseIndex] = (words, hiddenStates);
 
         if (indices.Count == 0)
         {
@@ -54,17 +41,6 @@ public class Scripture
 
         HideAvailableWords(indices, words);
     }
-    // GetRendered moving to Word
-    //public string GetRendered(int verseIndex)
-    //{
-    //    if (verseIndex < 0 ||verseIndex >= verses.Count)
-    //    {
-    //        throw new ArgumentOutOfRangeException("Invalid verse index");
-    //    }
-//
-    //    var (words, hiddenStates) = hiddenWords[verseIndex];
-    //    return string.Join(" ", words);
-    //}
     public string GetRendered(int verseIndex)
     {
         if (verseIndex <0 || verseIndex >= verses.Count)
@@ -83,7 +59,6 @@ public class Scripture
 
             int wordsThatAreLeft = Math.Min(3, indices.Count);
             List<int> selectedIndices = indices.OrderBy(x => random.Next()).Take(wordsThatAreLeft).ToList();
-            //moving this foreach loop to Word
             foreach (int index in selectedIndices)
             {
                 words[index].Hide();
@@ -97,9 +72,7 @@ public class Scripture
             throw new ArgumentOutOfRangeException("Invalid verse index");
         }
 
-        //var (_, hiddenStates) = hiddenWords[verseIndex];
         var words = hiddenWords[verseIndex];
-        //return hiddenStates.All(state => state);
         return words.All(word => word.IsHidden());
     }
 }
